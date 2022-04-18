7431aa354cbd0866970449f8fd25a10a4ccea8f0
Juan Manuel Velez Parra <jmvelez@itc.com.co>
2022-04-17 21:13:13 -0500

    backend

--- server/app.ts
+++ server/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import morgan from 'morgan';
-import cors from 'cors';
+import morgan from "morgan";
+import cors from "cors";
 import tasksRoutes from "./routes/index";
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', tasksRoutes)
+app.use("/api", tasksRoutes);
 
 export default app;
--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,10 +1,64 @@
-import { Router } from 'express';
+import { Router } from "express";
+import Task from "../models/Task";
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello world!');
+/* Hello World */
+router.get("/", (req, res) => {
+  res.send("Hello world!");
 });
 
+router.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  console.log(tasks);
+  res.send(tasks);
+});
+
+router.post("/tasks", async (req, res) => {
+  const { title, description } = req.body;
+  const task = new Task({ title, description });
+  console.log(task);
+  await task.save();
+
+  res.json(task);
+});
+
+router.get("/tasks/:id", async (req, res) => {
+  try {
+    console.log(req.params);
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    res.send(task);
+  } catch (error) {
+    console.log("Error with getting a task");
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    return res.json(task);
+  } catch (error) {
+    console.log("Error with deleting tasks");
+    console.log(error);
+    return res.status(500).send(error);
+  }
+});
+
+router.put("/tasks/:id", async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedTask);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 export default router;
